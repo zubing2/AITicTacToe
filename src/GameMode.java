f44c57a0c2a6cf8e70bc5c48daea946bc9e34793
Zubin Guan <zubing2@nycstudents.net>
2023-06-06 01:20:46 -0400

    broken, fix replay

--- src/GameMode.java
+++ src/GameMode.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class GameMode {
+import javax.swing.*;
+
+public class GameMode extends JDialog {
+    private boolean againstAI;
+
+    public GameMode(JFrame parent) {
+        super(parent, "Select Game Mode", true);
+
+        String[] options = {"Against AI", "Against Player"};
+        int choice = JOptionPane.showOptionDialog(
+                this,
+                "Select game mode:",
+                "Tic Tac Toe",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                options,
+                options[0]
+        );
+
+        againstAI = (choice == 0);
+        dispose();
+    }
+
+    public boolean isAgainstAI() {
+        return againstAI;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::startGame);
+    }
+
+    private static void startGame() {
+        GameMode dialog = new GameMode(null);
+        boolean againstAI = dialog.isAgainstAI();
+        new TicTacToeGame(againstAI);
+    }
 }
